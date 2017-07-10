843d23bf623f250af6e02f31b2c5dc3721eea6a0
sohini-roy <sohinirc45@gmail.com>
2017-07-10 16:31:16 +0530

    initial commit

--- index.js
+++ index.js
@@ -10,3 +10,43 @@ var _           = require('lodash');
 var git         = require('simple-git')();
 var touch       = require('touch');
 var fs          = require('fs');
+var files = require('./lib/files');
+// clear();
+// console.log(
+//   chalk.yellow(
+//     figlet.textSync('Ginit', { horizontalLayout: 'full' })
+//   )
+// );
+function getGithubCredentials(callback) {
+  var questions = [
+    {
+      name: 'username',
+      type: 'input',
+      message: 'Enter your Github username or e-mail address:',
+      validate: function( value ) {
+        if (value.length) {
+          return true;
+        } else {
+          return 'Please enter your username or e-mail address';
+        }
+      }
+    },
+    {
+      name: 'password',
+      type: 'password',
+      message: 'Enter your password:',
+      validate: function(value) {
+        if (value.length) {
+          return true;
+        } else {
+          return 'Please enter your password';
+        }
+      }
+    }
+  ];
+
+  inquirer.prompt(questions).then(callback);
+}
+getGithubCredentials(function(){
+  console.log(arguments);
+});
