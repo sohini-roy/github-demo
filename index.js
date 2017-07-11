1d889e6983d341431a631db69cd5d50be38b3199
sohini-roy <sohinirc45@gmail.com>
2017-07-11 15:38:37 +0530

    create OAuth

--- index.js
+++ index.js
@@ -11,12 +11,17 @@ var git         = require('simple-git')();
 var touch       = require('touch');
 var fs          = require('fs');
 var files = require('./lib/files');
-// clear();
+var prefs = new Preferences('git-set-state');
+var github = new GitHubApi({
+  version: '3.0.0'
+});
 // console.log(
 //   chalk.yellow(
 //     figlet.textSync('Ginit', { horizontalLayout: 'full' })
 //   )
 // );
+
+// function to get credentials
 function getGithubCredentials(callback) {
   var questions = [
     {
@@ -47,6 +52,56 @@ function getGithubCredentials(callback) {
 
   inquirer.prompt(questions).then(callback);
 }
+
+// returns credentials
 getGithubCredentials(function(){
   console.log(arguments);
 });
+
+// func to check if we already have an access token
+function getGithubToken(callback) {
+  var prefs = new Preferences('git-set-state');
+
+  if (prefs.github &amp;&amp; prefs.github.token) {
+    return callback(null, prefs.github.token);
+  }
+
+  // Fetch token
+    getGithubCredentials(function(credentials) {
+      ...
+    });
+  }
+
+  // authenticate with Github
+  getGithubCredentials(function(credentials) {
+    // Creating a spinner
+    var status = new Spinner('Authenticating you, please wait...');
+    status.start();
+    //  basic authentication prior to trying to obtain an OAuth token
+    github.authenticate(
+      _.extend(
+        {
+          type: 'basic',
+        },
+        credentials
+      )
+    );
+    // attempt to create an access token for our application
+    github.authorization.create({
+      scopes: ['user', 'public_repo', 'repo', 'repo:status'],
+      note: 'git-set-state, the command-line tool to set state for a commit in an issue'
+    }, function(err, res) {
+      // stop the spinner on success
+      status.stop();
+      if ( err ) {
+        return callback( err );
+      }
+      if (res.token) {
+        prefs.github = {
+          token : res.token
+        };
+        return callback(null, res.token);
+      }
+      return callback();
+    });
+  });
