c888ddb862fb034714c7f17beefdd75593ceb0b5
sohini-roy <sohinirc45@gmail.com>
2017-07-14 23:19:48 +0530

    minor changes

--- index.js
+++ index.js
@@ -24,138 +24,142 @@ var github = new GitHubApi({
 var username = '';
 // function to get credentials to create the OAuth token
 function getGithubCredentials(callback) {
-  var questions = [
-    {
-      name: 'username',
-      type: 'input',
-      message: 'Enter your Github username or e-mail address:',
-      validate: function( value ) {
-        if (value.length) {
-          return true;
-        } else {
-          return 'Please enter your username or e-mail address';
-        }
-      }
-    },
-    {
-      name: 'password',
-      type: 'password',
-      message: 'Enter your password:',
-      validate: function(value) {
-        if (value.length) {
-          return true;
-        } else {
-          return 'Please enter your password';
-        }
-      }
-    }
-  ];
-
-  inquirer.prompt(questions).then(function(answers){
-    username = answers.username;
-  });
 }
 
 // func to check if we already have an access token
 function getGithubToken(callback) {
   var prefs = new Preferences('git-set-state');
 
-  if (prefs.github &amp;&amp; prefs.github.token) {
+  if (prefs.github && prefs.github.token) {
     return callback(null, prefs.github.token);
   }
-
   // Fetch token
-  getGithubCredentials(function(credentials) {
-    // Creating a spinner
-    var status = new Spinner('Authenticating you, please wait...');
-    status.start();
-    //  basic authentication prior to trying to obtain an OAuth token
-    github.authenticate(
-      _.extend(
-        {
-          type: 'basic',
-        },
-        credentials
-      )
-    );
-    // attempt to specify scope for our application
-    github.authorization.create({
-      scopes: ['public_repo', 'repo', 'repo:status'],
-      note: 'git-set-state, the command-line tool to set state for a commit in an issue'
-    }, function(err, res) {
-      // stop the spinner on success
-      status.stop();
-      if ( err ) {
-        console.log(err);
-        return callback( err );
-      }
-      if (res.token) {
-        prefs.github = {
-          token : res.token
-        };
-        return callback(null, res.token);
-      }
-      return callback();
-    });
-  });
-
-  // function trying to set state for the Pull Request
-  function createStatus(callback) {
+  function getGithubCredentials(function(credentials) {
     var argv = require('minimist')(process.argv.slice(2));
 
     var questions = [
       {
+        name: 'username',
         type: 'input',
-        name: 'name',
-        message: 'Enter name of your repository:',
-        default: argv._[0] || files.getCurrentDirectoryBase(),
+        message: 'Enter your Github username or e-mail address:',
         validate: function( value ) {
           if (value.length) {
             return true;
           } else {
-            return 'Please enter the name of your repository';
+            return 'Please enter your username or e-mail address';
           }
         }
       },
       {
-        type: 'input',
-        name: 'pull_request_number',
-        message: 'Enter the '
-      },
-      {
-        type: 'input',
-        name: 'state',
-        message: 'Success/Error/Pending/Failure:',
-        choices: [ 'success', 'error', 'pending', 'failure' ],
-        default: 'pending'
-      },
-      {
-        type: 'input',
-        name: 'description',
-        default: argv._[1] || null,
-        message: 'Optionally enter a description for the Pull Request:'
+        name: 'password',
+        type: 'password',
+        message: 'Enter your password:',
+        validate: function(value) {
+          if (value.length) {
+            return true;
+          } else {
+            return 'Please enter your password';
+          }
+        }
       }
     ];
 
-    inquirer.prompt(questions).then(function(answers) {
-      var status = new Spinner('Creating repository...');
+    inquirer.prompt(questions).then(function(answers){
+      username = answers.username;
+      
+      // Creating a spinner
+      var status = new Spinner('Authenticating you, please wait...');
       status.start();
-
-      var data = {
-        name : answers.name,
-        description : answers.description,
-        private : (answers.visibility === 'private')
-      };
-
-      github.repos.create(
-        data,
-        function(err, res) {
-          status.stop();
-          if (err) {
-            return callback(err);
-          }
-          return callback(null, res.ssh_url);
-        }
+      //  basic authentication prior to trying to obtain an OAuth token
+      github.authenticate(
+        _.extend(
+          {
+            type: 'basic',
+          },
+          credentials
+        )
       );
+      // attempt to specify scope for our application
+      github.authorization.create({
+        scopes: ['public_repo', 'repo', 'repo:status'],
+        note: 'git-set-state, the command-line tool to set state for a commit in an issue'
+      }, function(err, res) {
+        // stop the spinner on success
+        status.stop();
+        if ( err ) {
+          console.log(err);
+          return callback( err );
+        }
+        if (res.token) {
+          prefs.github = {
+            token : res.token
+          };
+          return callback(null, res.token);
+        }
+        return callback();
+      });
     });
-  }
+  });
+}
+
+// function trying to set state for the Pull Request
+function createStatus(callback) {
+  var argv = require('minimist')(process.argv.slice(4));
+
+  var questions = [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'Enter name of your repository:',
+      default: argv._[0] || files.getCurrentDirectoryBase(),
+      validate: function( value ) {
+        if (value.length) {
+          return true;
+        } else {
+          return 'Please enter the name of your repository';
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'pull_request_number',
+      default: argv._[1] || null,
+      message: 'Enter the Pull Request Number'
+    },
+    {
+      type: 'input',
+      name: 'state',
+      message: 'Set the state - Success/Error/Pending/Failure:',
+      choices: [ 'success', 'error', 'pending', 'failure' ],
+      default: 'pending'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      default: argv._[2] || null,
+      message: 'Optionally enter a description for the Pull Request:'
+    }
+  ];
+
+  inquirer.prompt(questions).then(function(answers) {
+    var status = new Spinner('Creating status...');
+    status.start();
+
+    var data = {
+      name : answers.name,
+      description : answers.description,
+      private : (answers.visibility === 'private')
+    };
+
+    github.repos.create(
+      data,
+      function(err, res) {
+        status.stop();
+        if (err) {
+          return callback(err);
+        }
+        return callback(null, res.ssh_url);
+      }
+    );
+  });
+}
