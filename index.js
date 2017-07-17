9b9f44fcff6bd17c52cfc6567a61c35771c5b12c
sohini-roy <sohinirc45@gmail.com>
2017-07-17 23:39:59 +0530

    minor changes

--- index.js
+++ index.js
@@ -16,89 +16,35 @@ var github = new GitHubApi({
   version: '3.0.0'
 });
 
-var username = '';
-
-// func to check if we already have an access token
-// function getGithubToken() {
-  // var prefs = new Preferences('git-set-state');
-  //
-  // if (prefs.github && prefs.github.token) {
-  //   return callback(null, prefs.github.token);
-  // }
-  // Fetch token
-  function getGithubCredentials() {
-    var argv = require('minimist')(process.argv.slice(2));
-    console.log('l31');
-
-    var questions = [
-      {
-        name: 'username',
-        type: 'input',
-        message: 'Enter your Github username or e-mail address:',
-        validate: function( value ) {
-          if (value.length) {
-            return true;
-          } else {
-            return 'Please enter your username or e-mail address';
-          }
-        }
-      },
-      {
-        name: 'password',
-        type: 'password',
-        message: 'Enter your password:',
-        validate: function(value) {
-          if (value.length) {
-            return true;
-          } else {
-            return 'Please enter your password';
-          }
-        }
-      }
-    ];
-
-    inquirer.prompt(questions).then(function(answers){
-      username = answers.username;
-      console.log('l64');
-      // Creating a spinner
-      var status = new Spinner('Authenticating you, please wait...');
-      status.start();
-      //  basic authentication prior to trying to obtain an OAuth token
-      github.authenticate(
-        _.extend(
-          {
-            type: 'basic',
-          },
-          credentials
-        )
-      );
-      // attempt to specify scope for our application
-      github.authorization.create({
-        scopes: ['public_repo', 'repo', 'repo:status'],
-        note: 'git-set-state, the command-line tool to set state for a commit in an issue'
-      }, function(err, res) {
-        // stop the spinner on success
-        status.stop();
-        if ( err ) {
-          console.log(err);
-        }
-        if (res.token) {
-          prefs.github = {
-            token : res.token
-          };
-          // return callback(null, res.token);
-        }
-        // return callback();
-      });
-    });
-  }
-// }
-
 // function trying to set state for the Pull Request
 function createStatus() {
   var argv = require('minimist')(process.argv.slice(4));
 
   var questions = [
+    {
+      name: 'username',
+      type: 'input',
+      message: 'Enter your Github username or e-mail address:',
+      validate: function( value ) {
+        if (value.length) {
+          return true;
+        } else {
+          return 'Please enter your username or e-mail address';
+        }
+      }
+    },
+    {
+      name: 'password',
+      type: 'password',
+      message: 'Enter your password:',
+      validate: function(value) {
+        if (value.length) {
+          return true;
+        } else {
+          return 'Please enter your password';
+        }
+      }
+    },
     {
       type: 'input',
       name: 'name',
@@ -134,13 +80,44 @@ function createStatus() {
   ];
 
   inquirer.prompt(questions).then(function(answers) {
-    var status = new Spinner('Creating status...');
+    // Creating a spinner
+    var status = new Spinner('Authenticating you, please wait...');
     status.start();
+    //  basic authentication prior to trying to obtain an OAuth token
+    // github.authenticate(
+    //   _.extend(
+    //     {
+    //       type: 'basic',
+    //     },
+    //     // credentials
+    //   )
+    // );
+    // attempt to specify scope for our application
+    github.authorization.create({
+      scopes: ['public_repo', 'repo', 'repo:status'],
+      note: 'git-set-state, the command-line tool to set state for a commit in an issue'
+    }, function(err, res) {
+      // stop the spinner on success
+      status.stop();
+      if ( err ) {
+        console.log(err);
+      }
+      if (res.token) {
+        prefs.github = {
+          token : res.token
+        };
+        // return callback(null, res.token);
+      }
+      // return callback();
+    });
+    var statusInPr = new Spinner('Creating status...');
+    statusInPr.start();
 
     var data = {
-      owner : username,
+      sha : 123,
+      owner : answers.username,
       repo : answers.name,
-      target_url : "github.com/" + username + "/" + answers.name + "/pull/" + answers.pull_request_number,
+      target_url : "github.com/" + answers.username + "/" + answers.name + "/pull/" + answers.pull_request_number,
       description : answers.description,
       state : answers.state
     };
@@ -148,15 +125,18 @@ function createStatus() {
     github.repos.createStatus(
       data,
       function(err, res) {
-        status.stop();
+        statusInPr.stop();
         if (err) {
           console.log(err);
         }
       }
     );
+  })
+  .catch(function(error){
+      // status.stop();
+      // statusInPr.stop();
+    console.log(error);
   });
 }
 
-getGithubCredentials();
-console.log('l163');
 createStatus();
